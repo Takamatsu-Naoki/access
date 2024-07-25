39f8cbd5f3853226525b04982a199a81bc2a4a4b
Takamatsu Naoki <Takamatsu0331@outlook.jp>
2024-07-25 21:09:54 +0900

    :sparkles: feat: Design Homepage

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import './styles.css';
+	import icon from '$lib/resource/images/icon.png';
+</script>
+
+<svelte:head>
+	<link rel="icon" href={icon} />
+</svelte:head>
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,150 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>ACCESS: Accessible Coding and Computer Education for Students with Sight-impairment</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+		rel="stylesheet"
+	/>
+	<style>
+		html,
+		body {
+			width: 100%;
+			height: 100%;
+			overflow-y: hidden;
+		}
+
+		body::-webkit-scrollbar {
+			display: none;
+		}
+
+		.block-path {
+			stroke: #222222;
+			stroke-width: 1.3px;
+		}
+
+		.trigger-action-block-path {
+			fill: #f26627;
+		}
+
+		.action-block-path {
+			fill: #007ac4;
+		}
+
+		.condition-block-path {
+			fill: #3abe86;
+		}
+
+		.number-block-path {
+			fill: #ffd40f;
+		}
+
+		.string-block-path {
+			fill: #954a35;
+		}
+
+		.text {
+			fill: #222222;
+		}
+
+		.label-path {
+			fill: #f5f6f7;
+			stroke: #222222;
+			stroke-width: 0.5px;
+		}
+	</style>
+</svelte:head>
+
+<div id="hidden-div">
+	<svg id="hidden-SVG" />
+</div>
+<header id="menu-bar">
+	<p id="title">
+		ACCESS: Accessible Coding and Computer Education for Students with Sight-impairment
+	</p>
+</header>
+<div id="body-wrapper">
+	<div id="toolbox-wrapper">
+		<svg id="toolbox" viewBox="0 0 700 1400" />
+	</div>
+	<div id="workspace-wrapper">
+		<svg id="workspace" viewBox="0 0 700 1400" />
+	</div>
+</div>
+
+<style>
+	#hidden-div {
+		position: absolute;
+		left: 0;
+	}
+
+	#hidden-SVG {
+		position: absolute;
+		right: 0;
+	}
+
+	#menu-bar {
+		position: sticky;
+		z-index: 1;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 3rem;
+		display: flex;
+		align-items: center;
+	}
+
+	#body-wrapper {
+		height: calc(100% - 3rem);
+    min-width: 1600px;
+		padding: 12px;
+		display: flex;
+	}
+
+	#toolbox-wrapper {
+		width: 700px;
+		margin-right: 12px;
+		max-height: calc(100% - 24px);
+		background-color: #bfe4ff;
+		overflow-y: scroll;
+		white-space: nowrap;
+	}
+
+	#toolbox-wrapper::-webkit-scrollbar {
+		display: none;
+	}
+
+	#toolbox {
+		min-width: 100%;
+		min-height: 100%;
+	}
+
+	#workspace-wrapper {
+    flex-grow: 1;
+		max-height: calc(100% - 24px);
+		background-color: #bfe4ff;
+		overflow: scroll;
+		white-space: nowrap;
+	}
+
+	#workspace-wrapper::-webkit-scrollbar {
+		display: none;
+	}
+
+	#workspace {
+		min-width: 100%;
+		min-height: 100%;
+	}
+
+	#title {
+		position: absolute;
+		top: -10px;
+		left: 10px;
+		font-size: 1.5em;
+		color: white;
+		white-space: nowrap;
+	}
+</style>
