6d3bd6d1d6c168932a02d7b7893ddb9fdb260eb9
Takamatsu Naoki <Takamatsu0331@outlook.jp>
2024-09-01 11:21:20 +0900

    :sparkles: feat: Add Utility Functions

--- src/lib/code/fp-ts-utils/graph.ts
+++ src/lib/code/fp-ts-utils/graph.ts
@@ -1,4 +1,6 @@
+import type {Predicate} from 'fp-ts/Predicate';
 import { pipe } from 'fp-ts/function';
+import * as O from 'fp-ts/Option';
 import * as RM from 'fp-ts/ReadonlyMap';
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as N from 'fp-ts/number';
@@ -21,13 +23,39 @@ export const initializeGraph = <E, R>(): Graph<E, R> => ({
   links: []
 });
 
+export const findNodeById =
+  <E, R>(graph: Graph<E, R>) =>
+    (nodeId: number) =>
+      pipe(
+        graph.nodes,
+        RM.lookupWithKey(N.Eq)(nodeId),
+        O.map((a) => a[1])
+      );
+
+export const resolveNodeByLink =
+  <E, R>(graph: Graph<E, R>) =>
+    (nodeId: number) =>
+      (relation: R) =>
+        pipe(
+          graph.links,
+          RA.filter(a => a.subjectNodeId === nodeId && a.relation === relation),
+          RA.head,
+          O.map((a) => a.objectNodeId),
+        );
+
+export const filterNodes = <E, R>(graph: Graph<E, R>) => (predicate: Predicate<E>) => pipe(
+  graph.nodes,
+  RM.filter<E>(predicate),
+  RM.keys(N.Ord)
+)
+
 export const addNode =
   <E, R>(graph: Graph<E, R>) =>
-        (entity: E): Graph<E, R> => ({
-          ...graph,
-          nextNodeId: graph.nextNodeId + 1,
-          nodes: pipe(graph.nodes, RM.upsertAt(N.Eq)(graph.nextNodeId, entity)),
-        });
+    (entity: E): Graph<E, R> => ({
+      ...graph,
+      nextNodeId: graph.nextNodeId + 1,
+      nodes: pipe(graph.nodes, RM.upsertAt(N.Eq)(graph.nextNodeId, entity))
+    });
 
 export const appendNode =
   <E, R>(graph: Graph<E, R>) =>
@@ -36,10 +64,7 @@ export const appendNode =
         (entity: E): Graph<E, R> => ({
           nextNodeId: graph.nextNodeId + 1,
           nodes: pipe(graph.nodes, RM.upsertAt(N.Eq)(graph.nextNodeId, entity)),
-          links: pipe(
-            graph.links,
-            RA.append({ subjectNodeId, relation, objectNodeId: graph.nextNodeId })
-          )
+          links: pipe(graph.links, RA.append({ subjectNodeId, relation, objectNodeId: graph.nextNodeId }))
         });
 
 export const removeNode =
