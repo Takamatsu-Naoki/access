723259039df47bb99e00b308a92d5ea52486c6bb
Takamatsu Naoki <Takamatsu0331@outlook.jp>
2024-09-01 17:18:56 +0900

    :sparkles: feat: Add generateToolboxTable

--- src/lib/code/label-table.ts
+++ src/lib/code/label-table.ts
@@ -1,7 +1,9 @@
 import { pipe } from 'fp-ts/function';
+import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
+import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
 import { type Table, appendElement, appendEmptyRow } from '$lib/code/fp-ts-utils/table';
-import { isActionBlock, isTriggerActionBlock } from './svg/block';
+import { isActionBlock, isLabel, isTriggerActionBlock } from './svg/block';
 
 export type LabelTable = Table<Element>;
 
@@ -20,6 +22,15 @@ export const traverseElements = (table: LabelTable) => (element: Element) => {
   return isTriggerActionBlock(element) || isActionBlock(element) ? appendEmptyRow(table) : table;
 };
 
-export const generateWorkspaceTable = (workspace: Element) => {
-  return pipe(workspace, traverseElements([[]]), RA.filter(RA.isNonEmpty));
-};
+export const generateWorkspaceTable = (workspace: Element) =>
+  pipe(workspace, traverseElements([[]]), RA.filter(RA.isNonEmpty));
+
+export const generateToolboxTable = (toolbox: Element) =>
+  pipe(
+    Array.from(toolbox.children),
+    RA.map((block) => pipe(Array.from(block.children), RA.filter(isLabel), RA.head)),
+    RA.compact,
+    RA.map((a) => [a]),
+    RNEA.fromReadonlyArray,
+    O.getOrElse(() => [[]] as LabelTable)
+  );
