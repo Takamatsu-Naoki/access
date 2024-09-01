03781164178273fd4d0276ccf0d7e760b1860a74
Takamatsu Naoki <Takamatsu0331@outlook.jp>
2024-09-01 11:22:04 +0900

    :sparkles: feat: Add CodeGraph

---
+++ src/lib/code/code-graph.ts
@@ -0,0 +1,26 @@
+import type { Graph } from '$lib/code/graph';
+import { SymbolEntity } from '$lib/resource/graph/symbol-entity';
+import { SymbolRelation } from '$lib/resource/graph/symbol-relation';
+
+type NumericEntity = {
+  type: 'number';
+  value: number;
+};
+
+type StringEntity = {
+  type: 'string';
+  value: string;
+};
+
+export type CodeEntity = SymbolEntity | NumericEntity | StringEntity;
+
+export type CodeGraph = Graph<CodeEntity, SymbolRelation>;
+
+export const isSymbolEntity = (entity: CodeEntity): entity is SymbolEntity =>
+  typeof entity === 'string';
+
+export const isNumericEntity = (entity: CodeEntity): entity is NumericEntity =>
+  !isSymbolEntity(entity) && entity.type === 'number';
+
+export const isStringEntity = (entity: CodeEntity): entity is StringEntity =>
+  !isSymbolEntity(entity) && entity.type === 'string';
